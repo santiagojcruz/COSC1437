c8655e4c87454d06e248950b27de35bc20643247
santiagojcruz <157390909+santiagojcruz@users.noreply.github.com>
2024-02-05 18:47:45 +0000

    for 6.3

--- mod6_3.cpp
+++ mod6_3.cpp
@@ -7,6 +7,31 @@ int main()
     double tdv;
     double NOT;
     double NOS;
-    
+    double score;
+    int bonus;
+    cout << "What is your total dollar value? \n";
+    cin >> tdv;
+    cout << "What is your Number of Transactions\n";
+    cin >> NOT;
+    cout << "What is your Number of Shifts\n";
+    cin >> NOS;
+    score = (tdv/NOT)/NOS;
+    if (score <=30)
+    {
+        bonus = 50;
+    }
+    else if ((score > 30) && (score < 70))
+    {
+        bonus = 75;
+    }
+    else if ((score > 69) && (score < 200))
+    {
+        bonus = 100;
+    }
+    else if (score >= 200)
+    {
+        bonus = 200;
+    }
+    cout << "Your bonus will be $" << bonus << "\n";
     return 0;
 }
